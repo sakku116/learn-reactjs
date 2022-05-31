448839a9e588d3c54361cc4723dd477e867fab03
sakku_116 <88846365+sakku116@users.noreply.github.com>
2022-05-31 11:59:33 +0700

    .

--- freecodecamp youtube/first react/index.js
+++ freecodecamp youtube/first react/index.js
@@ -1,4 +1,30 @@
-// add children to '#root' element using react
+// menambahkan children pada element '#root'
+// tidak dapat menggunakan render berulang pada satu element seperti '#root'
 ReactDOM.render(<h1>Hello World!</h1>, document.querySelector('#root'))
 
-ReactDOM.render(<p>Hi, my name is zakky</p>, document.querySelector('#root'))
+/*
+function dibawah merupakan react element dan harus menggunakan kapital pada huruf pertama. 
+react element harus mereturn 1 element. jika lebih harus dibungkus dengan container
+*/ 
+function Element1 () {
+    return (
+        <h1>element 1</h1>
+    )
+}
+
+function Element2 () {
+    return (
+        <ul>
+            <li>thing 1</li>
+            <li>thing 2</li>
+        </ul>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <Element1 />
+        <Element2 />
+    </div>,
+    document.querySelector('#root')
+)
