5c08a806a0550aed4a904fbc3d5f7158bf8ef9f0
sakku_116 <88846365+sakku116@users.noreply.github.com>
2022-05-31 10:26:58 +0700

    first commit

---
+++ freecodecamp youtube/first react/index.js
@@ -0,0 +1,4 @@
+// add children to '#root' element using react
+ReactDOM.render(<h1>Hello World!</h1>, document.querySelector('#root'))
+
+ReactDOM.render(<p>Hi, my name is zakky</p>, document.querySelector('#root'))
