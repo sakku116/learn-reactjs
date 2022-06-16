fce0c24cf4355fcd774cc523589c8bbb74a84ded
sakku_116 <88846365+sakku116@users.noreply.github.com>
2022-06-16 14:04:00 +0700

    update

--- dicoding/statefull-component/src/index.js
+++ dicoding/statefull-component/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+/*
 import CountApp from './pages/CountApp';
 import ContactApp from './pages/ContactApp'
 import FormApp from './pages/FormApp';
+*/
+import NotesApp from './pages/NotesApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function Main () {
     return (
-        <ContactApp />
+        <BookshelfApp />
     )
 }
 
